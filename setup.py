68149c864d56668185cde452a70767dc031aea99
emmlejeail <manu.lejeail@gmail.com>
2017-11-20 12:01:24 +0100

    package-info: Remove os package from requirements and add coding Latin-1 for copyright symbol

--- setup.py
+++ setup.py
@@ -1,3 +1,4 @@
+# coding: Latin-1
 # Copyright © 2017 The Things Network
 # Use of this source code is governed by the MIT license that can be found in the LICENSE file.
 
@@ -24,7 +25,6 @@ setup(name='ttnmqtt',
           'grpcio',
           'protobuf',
           'google-api-python-client',
-          'google-cloud',
-          'os'
+          'google-cloud'
       ],
       zip_safe=False)
