8e328233c18be3c3ef922fe2e39f6bf7f77e9e90
emmlejeail <manu.lejeail@gmail.com>
2018-01-08 10:38:43 +0100

    test: Removed type of argument in mock class because feature not available in python 2.7

--- mock.py
+++ mock.py
@@ -19,37 +19,37 @@ class MockApplicationManager:
         self.deviceList = None
         self.err = None
 
-    def RegisterApplication(self, appid: proto.ApplicationIdentifier, timeout, meta):
+    def RegisterApplication(self, appid, timeout, meta):
         self.applicationIdentifier = appid
         return {"err": self.err}
 
-    def GetApplication(self, req: proto.ApplicationIdentifier, timeout: int, meta):
+    def GetApplication(self, req, timeout, meta):
         self.applicationIdentifier = req
         return {"application": self.application, "err": self.err}
 
-    def SetApplication(self, app: proto.Application, timeout, meta):
+    def SetApplication(self, app, timeout, meta):
         self.application = app
         return {"err": self.err}
 
-    def DeleteApplication(self, appid: proto.ApplicationIdentifier, timeout, meta):
+    def DeleteApplication(self, appid, timeout, meta):
         self.applicationIdentifier = appid
         return {"err": self.err}
 
-    def GetDevice(self, devid: proto.DeviceIdentifier, timeout, meta):
+    def GetDevice(self, devid, timeout, meta):
         self.deviceIdentifier = devid
         return self.device
 
-    def SetDevice(self, dev: proto.Device, timeout, meta):
+    def SetDevice(self, dev, timeout, meta):
         self.device = dev
         if self.deviceList == None:
             self.deviceList = proto.DeviceList()
         return {"err": self.err}
 
-    def DeleteDevice(self, devid: proto.DeviceIdentifier, timeout, meta):
+    def DeleteDevice(self, devid, timeout, meta):
         self.deviceIdentifier = devid
         self.device = None
         return {"err": self.err}
 
-    def GetDevicesForApplication(self, appid: proto.ApplicationIdentifier, timeout, meta):
+    def GetDevicesForApplication(self, appid, timeout, meta):
         self.applicationIdentifier = appid
         return self.deviceList
