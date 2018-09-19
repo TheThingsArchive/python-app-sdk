2ba8b5dea7c4f56094cb567583835dad9de07a9e
Eric Gourlaouen <eric@thethingsnetwork.org>
2018-09-19 16:42:39 +0200

    Release v2.1.4

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with io.open("README.rst", encoding="utf-8") as f:
     long_description = f.read()
 
 setup(name="ttn",
-      version="2.1.3",
+      version="2.1.4",
       description="The Things Network Client",
       long_description = long_description,
       url = "https://github.com/TheThingsNetwork/python-app-sdk",
